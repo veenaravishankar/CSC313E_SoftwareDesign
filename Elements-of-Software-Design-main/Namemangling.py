a27cd97f02f43ac9478062c6484d9fd4366fba24
veenaravishankar <veenaravishankar@users.noreply.github.com>
2024-09-22 17:39:03 -0500

    Update Namemangling.py

--- Elements-of-Software-Design-main/Namemangling.py
+++ Elements-of-Software-Design-main/Namemangling.py
@@ -14,12 +14,3 @@ class MyB(MyA):
          return self.__variable2
 
 ###access variables to understand name mangling!
-a = MyA()
-b = MyB()
-print(a._variable1, b._variable1)
-
-#print(a.__variable2) # gives you error!
-
-print(a.getvariable2(), b.getvariable2())
-
-#print(b.getvariable2_B())# gives you error!
