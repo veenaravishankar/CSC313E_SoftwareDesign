b13f65f737ccdfc829e9f898df216f0522fd8265
veenaravishankar <veenaravishankar@users.noreply.github.com>
2024-09-22 20:10:44 -0500

    Update Polymorphism.py

--- Elements-of-Software-Design-main/Polymorphism.py
+++ Elements-of-Software-Design-main/Polymorphism.py
@@ -8,7 +8,7 @@ class Lavender(Mint):
 	def display(self):
 		print("I'm a lavender")
 
-mint_1 = Mint()
-mint_1.display()
-lavender_1 = Lavender()
-lavender_1.display()
+leaf = Mint()
+leaf.display()
+leaf = Lavender()
+leaf.display()
